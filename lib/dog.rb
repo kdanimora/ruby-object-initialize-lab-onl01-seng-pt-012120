b53b1752d3f1cf5b94cc06a0121be1364ee27249
Karen Daniela <kdaniimora@gmail.com>
2020-02-08 22:20:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,29 +1,11 @@
 class Dog 
   
-  def initialize(name)
+  def initialize(name, breed="Mutt")
     @name = name 
+    @bred = breed 
   end
- 
-  def name=(name)
-    @name = name
-  end
-
-def name 
-  @name
 end 
 
-  def initialize(breed)
-    @breed = breed
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
-  end
- 
-end 
+
 
 
